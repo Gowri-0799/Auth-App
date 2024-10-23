83dd3abfc4486d0503416ede1d8f80639e50a326
Gowri-0799 <rubinisri77@gmail.com>
2024-10-23 10:36:18 +0530

    company info UI changes

--- resources/views/companyinfo.blade.php
+++ resources/views/companyinfo.blade.php
@@ -2,5 +2,60 @@
 @section('title', "Company Info")
 
 @section('content')
+<div id="content" style="box-sizing: border-box; margin-left:300px; width:100%" class="p-3">
+    <div class="mb-2 w-100">
+        <h2 class="mt-2 mb-5">Company Info</h2>
+    </div>
+    <form action="" method="POST" enctype="multipart/form-data">
+        @csrf
+        <div class="mb-4 row">
+            <div class="col-lg-2">
+                <label for="logo" class="form-label fw-bold">Logo*</label>
+            </div>
+            <div class="col-lg-6">
+                <label for="uploadImage" class="image-upload-btn text-primary fw-bold">Update Logo</label>
+                <input type="file" id="uploadImage" name="logo" accept="image/*" />
+                <span class="text-danger"> </span>
+            </div>
+        </div>
 
+        <div class="mb-4 row">
+            <div class="col-lg-4">
+                <label for="company_name" class="form-label fw-bold">Company name for display on site*</label>
+                <input name="company_name" class="form-control" value="" required>
+                <span class="text-danger"> </span>
+            </div>
+
+            <div class="col-lg-4 mb-4">
+                <label for="tune_link" class="form-label fw-bold">Tune Link*</label>
+                <input name="tune_link" value="" class="form-control" required>
+                <span class="text-danger"> </span>
+            </div>
+        </div>
+
+        <!-- Flexbox for aligned inputs -->
+        <div class="row">
+            <div class="col-lg-4 d-flex flex-column">
+                <label for="landing_page_url" class="form-label fw-bold">Landing Page Url* 
+                    <span class="body-text-small fw-normal">(The page where you want customers to land on your site.)</span>
+                </label>
+                <input name="landing_page_url" class="form-control" value="" required>
+                <span class="text-danger"> </span>
+                <p class="body-text-small mt-1">Enter the full URL, including 'http://' or 'https://'. For example, 'https://www.example.com'.</p>
+            </div>
+
+            <div class="col-lg-4 d-flex flex-column">
+                <label for="landing_page_url_spanish" class="form-label fw-bold">Landing Page Url (Spanish) 
+                    <span class="body-text-small fw-normal">(The page where you want Spanish-speaking customers to land on your site.)</span>
+                </label>
+                <input name="landing_page_url_spanish" class="form-control" value="">
+                <span class="text-danger"> </span>
+                <p class="body-text-small mt-1">Enter the full URL, including 'http://' or 'https://'. For example, 'https://www.example.com'.</p>
+            </div>
+        </div>
+
+        <input type="submit" class="btn btn-primary text-end text-white px-3" value="Update Data">
+        <a class="btn button-clearlink text-primary fw-bold ms-3" data-bs-toggle="modal" data-bs-target="#sendDetailModal"> Send Details To Admin</a>
+    </form>
+</div>
 @endsection
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,13 +2,13 @@
 <html lang="en">
 
 <head>
-<meta charset="UTF-8">
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title')</title>
     <link rel="icon" href="/assets/images/favicon-32x32.png" type="image/x-icon">
     <link href="https://cdn.lineicons.com/4.0/lineicons.css" rel="stylesheet">
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -29,14 +29,14 @@
         })(window, document, "clarity", "script", "n8x5ekx79q");
     </script>
 
-    <style> 
+    <style>
         .sidebar-item .submenu.show {
             display: block !important;
         }
 
         /* Apply Bootstrap's primary button color to active sidebar links */
         .sidebar-item .sidebar-link {
-            color: #6c757d; /* Default color for inactive links */
+           
             padding: 8px 16px; /* Padding for better spacing */
             border-radius: 50px; /* Rounded corners */
             transition: background-color 0.3s ease, color 0.3s ease, box-shadow 0.3s ease; /* Smooth transition */
